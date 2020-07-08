10b1a4f46db412b507843c83d497e7fc7e08b39c
Rasen <arch_yu@mail.ru>
2020-07-08 22:36:45 +0300

    Generic method

--- Generics/src/generics/Generics2Demo.java
+++ Generics/src/generics/Generics2Demo.java
@@ -1,14 +1,24 @@
+//Two types classes + generic methods
 package generics;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class Generics2Demo {
 	public static void main(String[] args) {
 		Storage2<String, Integer> storage = new Storage2<>(); // Optionally can write ..= new Storage<String>(); ->
 																// Diamond
 		storage.setObject1("Sweet home Alabama");
-		storage.setObject2(Integer.valueOf(23));
+		storage.setObject2(Integer.valueOf(50));
+
+		List<String> list = new ArrayList<>(); // Collection for generic method demo
+		list.add("West Virginia");
+		list.add(storage.getObject1());
 
 		System.out.println(storage.getObject1());
 		System.out.println(storage.getObject2());
+		System.out.println(storage.<String>getFirst(list));
 	}
 
 }
@@ -31,6 +41,11 @@ class Storage2<T, S> {
 
 	public void setObject2(S object2) {
 		this.object2 = object2;
+	}
+
+	// Generic method
+	public <C> C getFirst(Collection<C> col) {
+		return col.iterator().next();
 	}
 
 }
