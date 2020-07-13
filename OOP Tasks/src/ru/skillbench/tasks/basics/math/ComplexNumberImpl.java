60adf5a30333fccc69293d4612eba3fbf1d722fe
Rasen <arch_yu@mail.ru>
2020-07-14 00:28:33 +0300

    Deleted garbage comments

--- OOP Tasks/src/ru/skillbench/tasks/basics/math/ComplexNumberImpl.java
+++ OOP Tasks/src/ru/skillbench/tasks/basics/math/ComplexNumberImpl.java
@@ -38,7 +38,6 @@ public class ComplexNumberImpl implements ComplexNumber, Comparable<ComplexNumbe
 
 		if (!value.startsWith("+") && !value.startsWith("-")) {
 			value = "+" + value;
-			System.out.println("New value = " + value);
 		}
 
 		int lastSignIndex; // firstSingIndex would always be 0
@@ -48,13 +47,10 @@ public class ComplexNumberImpl implements ComplexNumber, Comparable<ComplexNumbe
 			lastSignIndex = value.lastIndexOf('-');
 		}
 
-		System.out.println("last sign index = " + lastSignIndex);
-
 		if (lastSignIndex == 0) {
 			// 1 summand
 			if (value.endsWith("i")) {
 				// 1 imaginary summand
-				System.out.println("ends with i");
 				re = 0.0;
 				try {
 					im = Double.parseDouble(value.substring(0, value.length() - 1));
@@ -68,21 +64,18 @@ public class ComplexNumberImpl implements ComplexNumber, Comparable<ComplexNumbe
 						im = -1.0;
 						break;
 					}
+					return;
 				}
-				System.out.println("Set re = 0, im = double");
 			} else {
 				// 1 real summand
-				System.out.println("does NOT end with i");
 				im = 0.0;
 				re = Double.parseDouble(value);
-				System.out.println("Set re = double, im = 0");
+				return;
 			}
 		} else {
 			// 2 summands
-			System.out.println("Re = parsing from " + value.substring(0, lastSignIndex));
 			re = Double.parseDouble(value.substring(0, lastSignIndex));
 			try {
-				System.out.println("Im = parsing from " + value.substring(lastSignIndex, value.length() - 1));
 				im = Double.parseDouble(value.substring(lastSignIndex, value.length() - 1));
 			} catch (NumberFormatException e) {
 				// .. +i or .. -i
@@ -94,119 +87,10 @@ public class ComplexNumberImpl implements ComplexNumber, Comparable<ComplexNumbe
 					im = -1.0;
 					break;
 				}
+				return;
 			}
 		}
 	}
-//		if (value.startsWith("i") || value.startsWith("+i")) {
-//			re = 0.0;
-//			im = 1.0;
-//			return;
-//		}
-//
-//		if (value.startsWith("-i")) {
-//			re = 0.0;
-//			im = -1.0;
-//			return;
-//		}
-
-//		Double first = 0.0, second = 0.0;
-//		int index = 0; // index of sign (if there is one)
-//
-//		try {
-//			first = Double.valueOf(value);
-//			System.out.println("first = " + first);
-//			re = first;
-//		} catch (NumberFormatException e) {
-//			System.out.println("If here, then re = 0, im: +i -i or i");
-//			switch (value.charAt(0)) {
-//			case '+':
-//			case 'i':
-//				re = 0.0;
-//				im = 1.0;
-//				return;
-//			case '-':
-//				re = 0.0;
-//				im = -1.0;
-//				return;
-//			}
-//		}
-//		System.out.println("After catch first");
-//		try {
-//			index = value.lastIndexOf('+');
-//			if (value.lastIndexOf('-') > index) {
-//				index = value.lastIndexOf('-');
-//			}
-//			System.out.println("last index of sign = " + index);
-//			System.out.println("subString = " + value.subSequence(index, value.length()).toString());
-//			if (index > 0) {
-//				System.out.println("index > 0");
-//				second = Double.valueOf(value.subSequence(index, value.length()).toString());
-//				System.out.println("second = " + second);
-//				im = second;
-//			} else {
-//				im = 0.0;
-//			}
-//		} catch (NumberFormatException e) {
-//			// If here, then re = 0, im: +i -i or i
-//			switch (value.charAt(index)) {
-//			case '+':
-//			case 'i':
-//				System.out.println("cought re + i");
-//				im = 1.0;
-//				return;
-//			case '-':
-//				im = -1.0;
-//				return;
-//			}
-//		}
-//		System.out.println("After catch second");
-//
-//	}
-
-//
-//		int signMod[] = { 1, 1 };
-//
-//		if (value.startsWith("-")) {
-//			signMod[0] = -1;
-//		}
-//
-//		int countMinus = value.length() - value.replaceAll("-", "").length();
-//
-////		countMinus =	\	0	 1	 2
-////		signMod[0] =  (+)1  1	-1	 -
-////		signMod[1] = (-)-1	-	 1	-1
-//
-//		switch (countMinus) {
-//		case 0:
-//			signMod[1] = 1;
-//			break;
-//		case 1:
-//			signMod[1] = -signMod[0];
-//			break;
-//		case 2:
-//			signMod[1] = -1;
-//			break;
-//		}
-//
-//		String[] tokens = value.split("[\\+-]");
-//		for (int i = 0; i < tokens.length; i++) {
-//			System.out.println("token = " + tokens[i]);
-//			if (tokens[i].equals("")) {
-//				re = 0.0;
-//				continue;
-//			}
-//			if (tokens[i].contains("i")) {
-//				if (tokens[i].length() > 1) {
-//					im = signMod[i] * Double.parseDouble(tokens[i]);
-//				} else {
-//					im = 1.0 * signMod[i];
-//				}
-//			} else {
-//				re = signMod[i] * Double.parseDouble(tokens[i]);
-//			}
-//		}
-//
-//	}
 
 	@Override
 	public ComplexNumber copy() {
@@ -297,18 +181,11 @@ public class ComplexNumberImpl implements ComplexNumber, Comparable<ComplexNumbe
 		return this;
 	}
 
-//	@Override
-//	protected Object clone() throws CloneNotSupportedException {
-//		// TODO Auto-generated method stub
-//		return super.clone();
+//	public static void main(String[] args) {
+//		ComplexNumberImpl number = new ComplexNumberImpl();
+//		number.set("i");
+//		System.out.println("object re = " + number.re);
+//		System.out.println("object im = " + number.im);
+//		System.out.println(number.toString());
 //	}
-
-	public static void main(String[] args) {
-		ComplexNumberImpl number = new ComplexNumberImpl();
-		number.set("");
-		System.out.println("object set");
-		System.out.println("object re = " + number.re);
-		System.out.println("object im = " + number.im);
-		System.out.println(number.toString());
-	}
 }
